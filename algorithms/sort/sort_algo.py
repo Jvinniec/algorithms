0dcd873c56d891465259cf1f453dd6596c73362b
Josh Cardenzana <jcardenzana@irap.omp.eu>
2020-03-11 10:48:44 +0100

    Update sorting algorithm testing

--- algorithms/sort/sort_algo.py
+++ algorithms/sort/sort_algo.py
@@ -15,14 +15,46 @@ class sort_algo:
 
         Parameters
         ----------
-        arr : array
+        `arr` : array
             Python list of sortable objects
 
         Returns
         -------
-        Sorted Python list
+        Input `arr` or Sorted Python list in inherited implementations
         """
         return arr
+
+    
+    def Test(self, arr, expected):
+        """ Method for testing a given sorting algorithm
+
+        Parameters
+        ----------
+        `arr` : list
+            Python list or array like object
+        `expected` : list
+            Python array like object representing the sorted version of `arr`
+
+        Returns
+        -------
+        True if sorting method works, otherwise false
+        """
+        # Make sure inputs are valid
+        if len(arr) != len(expected):
+            raise ValueError(f'arr length {len(arr)} not equal to expected \
+                               length {len(expected)}')
+
+        # Actually sort the array
+        sorted = self.sort(arr)
+
+        # Make sure the elements match
+        is_sorted = True
+        for i,val in enumerate(sorted):
+            if val != expected[i]:
+                is_sorted = False
+                break
+        
+        return is_sorted
 
 
 # Import the sorting algorithms
---
+++ algorithms/sort/tests/test.py
@@ -0,0 +1,71 @@
+# Make sure the sorting directories are findable
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
+import numpy as np
+import timeit
+
+# For generating random lists
+from random import randint
+
+# Import algorithms
+import sort_algo as sorter
+
+def test_data(entries=1000):
+    """ Generates a sample of random numbers and the expected sorted array
+    """
+    # Create an empty array to be filled
+    vals = np.zeros(entries)
+    for i in range(entries):
+        vals[i] = randint(-1000,1000)
+
+    return vals, np.sort(vals)
+
+
+def print_status(name, success, time):
+    """ Print the status of an algorithm
+    """
+    print(f' {name:9s} | {str(success):5s}   | {time:1.2e}')
+
+
+def run_tests():
+    """ Run tests on all Python sorting algorithm implementations
+    """
+    # Define the sorting algorithms to test
+    sort_algos = {'Numpy'     : np,
+                  'Bubble'    : sorter.bubble_sort(),
+                  'Selection' : sorter.selection_sort(),
+                  'Insertion' : sorter.insertion_sort(),
+                  'Heap'      : sorter.heap_sort(),
+                  'Merge'     : sorter.merge_sort()}
+
+    # Print some helpful text to tell us what's going on
+    print('TESTING: Sorting algorithms:')
+    print(f'-----------+---------+-----------')
+    print(f' Algo      | sorted? | time (sec)')
+    print(f'-----------+---------+-----------')
+
+    # Loop over all algorithms
+    for name,algo in sort_algos.items():
+        # Generate random unsorted array
+        test_arr, expected_arr = test_data()
+        
+        # Try to sort the array
+        sorted_arr = algo.sort(test_arr)
+        success = sorter.sort_algo().Test(sorted_arr, expected_arr)
+        
+        # If successfull test computation time of sorting algorithm
+        time = 0
+        if success:
+            time = timeit.timeit('test_arr, expected=test_data(); algo.sort(test_arr)', 
+                                 globals={'algo':algo,'test_data':test_data}, number=100)
+        
+        # Print success/failure
+        print_status(name, success, time)
+    
+    # Print a new line character
+    print()
+
+
+if __name__ == '__main__':
+    run_tests()
